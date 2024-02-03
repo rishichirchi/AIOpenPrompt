e088b3a2be5dc10682ffde5766fc98ff866b479c
Rishi Chirchi <rishiraj.chirchi@gmail.com>
2024-02-03 18:52:54 +0530

    set colorscheme for app

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return HomePage();
+    return MaterialApp(
+      theme: theme,
+      home: const HomePage(),
+    );
   }
 }
+
+final colorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 34, 40, 49),
+  background: const Color.fromARGB(255, 57, 62, 70),
+  primary: const Color.fromARGB(255, 0, 173, 181),
+  secondary: const Color.fromARGB(255, 238, 238, 238),
+);
+
+final theme = ThemeData().copyWith(
+    colorScheme: colorScheme,
+    scaffoldBackgroundColor: colorScheme.background,
+    textTheme: const TextTheme().apply(
+      bodyColor: colorScheme.primary,
+    ));
